de2e2cebd88a94145559e48f563f6d577c0d2645
mjernil <jernil@preimage.ai>
2024-10-25 11:42:41 +0530

    code cleanup, cosmetic updates & prettier config added

--- src/App.tsx
+++ src/App.tsx
@@ -1,115 +1,116 @@
-import { useState } from "react";
-import { Button, Flex } from "antd";
-import "./App.css";
-import Viewer from "./components/Viewer";
-import { Typography, Input } from "antd";
-import { PlusCircleOutlined } from "@ant-design/icons";
-import Socials from "./components/Socials";
+import { useState } from 'react';
+
+import { CloudUploadOutlined, PlusCircleOutlined, UploadOutlined } from '@ant-design/icons';
+import { Button, ConfigProvider, Flex } from 'antd';
+import { Input, Typography } from 'antd';
+import { createStyles } from 'antd-style';
+
+import Socials from './components/Socials';
+import Viewer from './components/Viewer';
+
+import './App.css';
+
+const useStyle = createStyles(({ prefixCls, css }) => ({
+    linearGradientButton: css`
+        &.${prefixCls}-btn-primary:not([disabled]):not(.${prefixCls}-btn-dangerous) {
+            border-width: 0;
+
+            > span {
+                position: relative;
+            }
+
+            &::before {
+                content: '';
+                background: linear-gradient(135deg, #b993d6, #8ca6db);
+                position: absolute;
+                inset: 0;
+                opacity: 1;
+                transition: all 0.3s;
+                border-radius: inherit;
+            }
+
+            &:hover::before {
+                opacity: 0.85;
+            }
+        }
+    `,
+}));
 
 function App() {
-  const [viewGS, setViewGS] = useState(false);
-  const [loadGLB, setLoadGLB] = useState(false);
+    const [viewGS, setViewGS] = useState(false);
 
-  const [url, setUrl] = useState("");
-  const [load, setLoad] = useState(false);
+    const [url, setUrl] = useState('');
+    const [load, setLoad] = useState(false);
 
-  const { Title } = Typography;
-  const { Search } = Input;
+    const { Title } = Typography;
+    const { styles } = useStyle();
 
-  return (
-    <>
-      <div style={{ position: "absolute", top: 1, left: 25, zIndex: 2 }}>
-        <Flex gap="small" align="start" vertical>
-          <Title className="app-title" level={2}>
-            Web Render Engine
-          </Title>
-          <Button
-            className="btn-grad"
-            size="large"
-            onClick={() => setViewGS(true)}
-            type={undefined}
-          >
-            <PlusCircleOutlined />
-            {"Load Splat model"}
-          </Button>
-          {viewGS && (
-            <>
-              <Search
-                value={url}
-                onChange={
-                  load
-                    ? (e) => {
-                        setLoad(false);
-                        setUrl(e.target.value);
-                      }
-                    : (e) => setUrl(e.target.value)
-                }
-                placeholder="Enter .splat url"
-                enterButton="Load"
-                onSearch={() => setLoad(true)}
-              />
-              <Button
-                color="grey"
-                onClick={() => {
-                  setUrl(
-                    "https://huggingface.co/cakewalk/splat-data/resolve/main/nike.splat"
-                  );
-                  setLoad(true);
-                }}
-                type="primary"
-              >
-                {"Load Sample Splat - Shoe"}
-              </Button>
-            </>
-          )}
-          {/* <Button
-            className="btn-grad"
-            size="large"
-            onClick={() => setLoadGLB(!loadGLB)}
-            type="secondary"
-          >
-            <PlusCircleOutlined />
-            {"Add glTF model"}
-          </Button>
-          {loadGLB && (
-            <>
-              <Search
-                value={url}
-                onChange={
-                  load
-                    ? (e) => {
-                        setLoad(false);
-                        setUrl(e.target.value);
-                      }
-                    : (e) => setUrl(e.target.value)
-                }
-                placeholder="Enter glTF url"
-                enterButton="Load"
-                onSearch={() => setLoad(true)}
-              />
-              <Button
-                color="grey"
-                onClick={() => {
-                  setUrl(
-                    "https://huggingface.co/cakewalk/splat-data/resolve/main/nike.splat"
-                  );
-                  setLoad(true);
-                }}
-                type="primary"
-              >
-                {"Load Sample GLB - Duck"}
-              </Button>
-            </>
-          )} */}
-        </Flex>
-      </div>
-      <div style={{ background: "white", width: "100vw", height: "100vh" }}>
-        <Viewer viewGS={viewGS} url={url} load={load} />
-      </div>
-      {/* You can remove this section away for your own projects! */}
-      <Socials />
-    </>
-  );
+    return (
+        <>
+            <div style={{ position: 'absolute', top: 1, left: 25, zIndex: 2 }}>
+                <ConfigProvider
+                    button={{
+                        className: styles.linearGradientButton,
+                    }}
+                >
+                    <Flex gap="small" align="start" vertical>
+                        <Title className="app-title" level={3}>
+                            Web Render Engine
+                        </Title>
+                        <Button
+                            type="primary"
+                            onClick={() => setViewGS(!viewGS)}
+                            icon={<PlusCircleOutlined />}
+                        >
+                            {viewGS ? 'Load Sample GLB' : 'Load Splat model'}
+                        </Button>
+                        {viewGS && (
+                            <>
+                                <Flex gap="small">
+                                    <Input
+                                        placeholder="Enter .splat url"
+                                        value={url}
+                                        onChange={
+                                            load
+                                                ? (e) => {
+                                                      setLoad(false);
+                                                      setUrl(e.target.value);
+                                                  }
+                                                : (e) => setUrl(e.target.value)
+                                        }
+                                    />
+                                    <Button
+                                        icon={<CloudUploadOutlined />}
+                                        type="primary"
+                                        onClick={() => setLoad(true)}
+                                    >
+                                        {'Load '}
+                                    </Button>
+                                </Flex>
+                                <Button
+                                    type="primary"
+                                    onClick={() => {
+                                        setUrl(
+                                            'https://huggingface.co/cakewalk/splat-data/resolve/main/nike.splat',
+                                        );
+                                        setLoad(true);
+                                    }}
+                                    icon={<UploadOutlined />}
+                                >
+                                    {'Load Sample - Shoe'}
+                                </Button>
+                            </>
+                        )}
+                    </Flex>
+                </ConfigProvider>
+            </div>
+            <div style={{ background: 'white', width: '100vw', height: '100vh' }}>
+                <Viewer viewGS={viewGS} url={url} load={load} />
+            </div>
+            {/* You can remove this section away for your own projects! */}
+            <Socials />
+        </>
+    );
 }
 
 export default App;
