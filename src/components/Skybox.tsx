402c9ba46227ee2a1f3a3a55a4db0f86b08559c9
Michael Jernil <43872457+mikejernil@users.noreply.github.com>
2024-11-04 14:49:30 +0530

    Revert "Revert "Implemented Minimalist UI with Dark Theme and Enhanced Upload Functionality""

--- src/components/Skybox.tsx
+++ src/components/Skybox.tsx
@@ -1,50 +1,54 @@
-import { extend, useLoader } from "@react-three/fiber";
-import { BackSide, LinearFilter, MathUtils, ShaderLib, Vector3 } from "three";
-import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
-import { Sky } from "three/examples/jsm/objects/Sky";
+import { BackSide, LinearFilter, MathUtils, ShaderLib, Vector3 } from 'three';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+import { Sky } from 'three/examples/jsm/objects/Sky';
+
+import { extend, useLoader } from '@react-three/fiber';
+
+import { useTheme } from '../context/ThemeContext';
 
 extend({ Sky });
 
 const HDRISky = () => {
-  const texture = useLoader(
-    RGBELoader,
-    "/assets/kloofendal_28d_misty_puresky_2k.hdr"
-  );
+    //darkmode
+    const { theme } = useTheme();
 
-  texture.magFilter = LinearFilter;
-  texture.minFilter = LinearFilter;
-  const shader = ShaderLib.equirect;
+    const hdrPath = `/assets/${theme === 'dark' ? 'kloppenheim_02_puresky_2k' : 'kloofendal_28d_misty_puresky_2k'}.hdr`;
 
-  return (
-    <mesh>
-      <boxGeometry args={[100, 100, 100]} />
-      <shaderMaterial
-        fragmentShader={shader.fragmentShader}
-        vertexShader={shader.vertexShader}
-        uniforms={shader.uniforms}
-        uniforms-tEquirect-value={texture}
-        depthWrite={false}
-        side={BackSide}
-      />
-    </mesh>
-  );
+    const texture = useLoader(RGBELoader, hdrPath);
+    texture.magFilter = LinearFilter;
+    texture.minFilter = LinearFilter;
+    const shader = ShaderLib.equirect;
+
+    return (
+        <mesh>
+            <boxGeometry args={[100, 100, 100]} />
+            <shaderMaterial
+                fragmentShader={shader.fragmentShader}
+                vertexShader={shader.vertexShader}
+                uniforms={shader.uniforms}
+                uniforms-tEquirect-value={texture}
+                depthWrite={false}
+                side={BackSide}
+            />
+        </mesh>
+    );
 };
 
 const ShaderSky = () => {
-  const sun = new Vector3();
-  const elevation = 90;
-  const azimuth = -150;
-  const phi = MathUtils.degToRad(90 - elevation);
-  const theta = MathUtils.degToRad(azimuth);
+    const sun = new Vector3();
+    const elevation = 90;
+    const azimuth = -150;
+    const phi = MathUtils.degToRad(90 - elevation);
+    const theta = MathUtils.degToRad(azimuth);
 
-  sun.setFromSphericalCoords(1, phi, theta);
+    sun.setFromSphericalCoords(1, phi, theta);
 
-  //@ts-expect-error undefined type error sky from drei
-  return <sky />;
+    //@ts-expect-error undefined type error sky from drei
+    return <sky />;
 };
 
-const Skybox = (props: { type: "SHADER" | "HDRI" }) => {
-  return props.type == "SHADER" ? <ShaderSky /> : <HDRISky />;
+const Skybox = (props: { type: 'SHADER' | 'HDRI' }) => {
+    return props.type == 'SHADER' ? <ShaderSky /> : <HDRISky />;
 };
 
 export default Skybox;
